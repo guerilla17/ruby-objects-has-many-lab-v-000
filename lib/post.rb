bea59943829c0fbc6bf978caaa1bc1f1e065dd30
Gigi M <mmgamao1@gmail.com>
2019-03-28 01:50:25 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,10 @@
-class Post  
-  attr_accessor :title, :author  
+cclass Post
+  attr_accessor :title, :author
 
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
 
@@ -12,20 +12,7 @@ class Post
     @@all
   end
 
-  def add_post(post)
-    post.author = self
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
-  end
-
-  def posts
-    Post.all.select {|post| post.author == self}
-  end
-
-  def self.post_count
-    Post.all.count
+  def author_name
+    author.name if author
   end
 end
